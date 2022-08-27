bdbd6b80f680db1104e3c61a010af5434f8b51e6
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-27 23:57:23 +0300

    Update Inequality Operator.js

--- Inequality Operator.js
+++ Inequality Operator.js
@@ -1,5 +1,10 @@
-1 !=  2    // true
-1 != "1"   // false
-1 != '1'   // false
-1 != true  // false
-0 != false // false
+//Оператор неравенства
+
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
