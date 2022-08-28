af3e935b660c93922dff2dd246d2ddc016038520
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-29 00:05:03 +0300

    Create logical operator.js

---
+++ logical operator.js
@@ -0,0 +1,12 @@
+function testLogicalAnd(val) {
+ /*  if (val >= 25) {
+    if (val <= 50) {*/
+  if (val >= 25 && val <= 50) {
+     return "Yes";
+    }
+  return "No";
+}
+
+testLogicalAnd(10);
+
+
