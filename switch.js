1a670398d4915eeb86e008c023d42c9e247c21b2
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-30 00:12:30 +0300

    Update switch.js

--- switch.js
+++ switch.js
@@ -23,3 +23,28 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
+
+//Добавление параметра поумолчанию в оператор switch. default оператор - выполняется если не найдено соответствующее case выражение
+function switchOfStuff(val) {
+  let answer = '';
+  // Only change code below this line
+  switch(val) {
+  case 'a':
+    answer = 'apple';
+    break;
+  case 'b':
+    answer = 'bird';
+    break;
+  case 'c':
+    answer = 'cat';
+    break;
+    default://Если не подходит ни один case
+    answer = 'stuff';
+    break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
