f062ce3340bc8c9cfa57b69ba1ee7e27e92abaf6
Cheloyek <79744034+Cheloyek@users.noreply.github.com>
2022-08-30 23:06:13 +0300

    Update switch.js

--- switch.js
+++ switch.js
@@ -48,3 +48,28 @@ function switchOfStuff(val) {
 }
 
 switchOfStuff(1);
+
+//Несколько одинаковых значений
+unction sequentialSizes(val) {
+  let answer = "";
+switch (val) {
+ case 1:
+ case 2:
+ case 3:
+  return "Low"
+  break
+ case 4:
+ case 5:
+ case 6:
+  return "Mid"
+  break
+ case 7:
+ case 8:
+ case 9:
+  return "High"
+  break
+}
+  return answer;
+}
+
+sequentialSizes(1);
